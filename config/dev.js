8c45f4ae6426cf04436b016573985f26c8ea272e
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2021-09-21 15:56:59 +0300

    a

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "904726263959-7rmiu0q2vftus1q9iodns9ihlh2q0222.apps.googleusercontent.com",
+  googleClientSecret: "QSy7gFSN7kkpKxxtbZWDdE9I",
+  mongoURI:
+    "mongodb+srv://pini85:Q16DdbVyAiQgeG83@cluster0.mf3qt.mongodb.net/find-my-movie?retryWrites=true&w=majority",
+  cookieKey: "tfgwe4y634y754uyh56jh46u6ur6tgfy6hgu67h",
+};
